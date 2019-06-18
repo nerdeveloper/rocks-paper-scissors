4b06584aa6054cccd41c86f170bf1c9c2de7edfa
Obinna Odirionye <odirionye@gmail.com>
2019-06-18 23:45:35 +0100

    update package file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-const world = '🗺️';
-
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
-}
-
-let name: string
-name = "Obinna",
-console.log(`${name}`);
+let player = 'rock';
+let otherPlayers: string[] = [''];
