d46a8841b63bdcd3ddb08eef3c1d0f984efa2ea2
Obinna Odirionye <odirionye@gmail.com>
2019-06-19 10:50:43 +0100

    add code commenting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+// Declate varible names
 let player = 'rock';
 let otherPlayers: string[] = ['paper', 'scissors'];
 
+// Declare the index of the loop
 let i = 1;
 
+// Check to see if the winner between ROCL and any other play
 export const checkMatch = (player: string, randomPlayer: string) => {
-    if (player === 'rock' && randomPlayer === 'scissors') {
+    if (player === 'rock' && randomPlayer === 'scissors') { // conditional statement
         return 'rock wins';
     } else {
         if (randomPlayer === 'paper') {
@@ -12,19 +15,24 @@ export const checkMatch = (player: string, randomPlayer: string) => {
         }
     }
 };
+
+// Display the output of the function in a string
 export const checkPlayers = (player: string, randomPlayer: string) => {
     return console.log(`${player} + ${randomPlayer} = ${checkMatch(player, randomPlayer)}`);
 };
 
+//Run a loop 100s over 100 loops to enhance performace and low consumption to memory
 export const myLoop = () => {
     setTimeout(function() {
+        // Picks player at random
         let randomPlayer = otherPlayers[Math.floor(Math.random() * otherPlayers.length)];
 
         checkPlayers(player, randomPlayer);
+
         i++;
         if (i <= 100) {
-            myLoop();
+            myLoop(); //Run a loop function if looping has not reach 100
         }
     }, 100);
 };
-myLoop();
+myLoop(); // run the loop again after 100 sec of delay
