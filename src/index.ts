faa41b871f4b2ddd3b3a35f8d31646a81d59be06
Obinna Odirionye <odirionye@gmail.com>
2019-06-19 09:33:31 +0100

    refactor(make functions exportable): this is to make functions exportable for testing

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 
-export module rocksPaperScissors {
 let player = 'rock';
 let otherPlayers: string[] = ['paper', 'scissors'];
 
 let i = 1;
 
-let myLoop = () => {
+export const  checkMatch = (player: string, randomPlayer: string) => {
+    if (player === 'rock' && randomPlayer === 'scissors') {
+        return 'rock wins';
+    } else {
+        if (randomPlayer === 'paper') {
+            return 'Rock losses';
+        }
+    }
+};
+export const  checkPlayers = (player: string, randomPlayer: string) => {
+    
+    return console.log(`${player} + ${randomPlayer} = ${checkMatch(player, randomPlayer)}`);
+};
+
+export const myLoop = () => {
     setTimeout(function() {
+      
         let randomPlayer = otherPlayers[Math.floor(Math.random() * otherPlayers.length)];
 
-        let checkPlayers = (player: string, randomPlayer: string) => {
-            let checkMatch = (player: string, randomPlayer: string) => {
-                if (player === 'rock' && randomPlayer === 'scissors') {
-                    return 'rock wins';
-                } else {
-                    if (randomPlayer === 'paper') {
-                        return 'Rock losses';
-                    }
-                }
-            };
-            return console.log(`${player} + ${randomPlayer} = ${checkMatch(player, randomPlayer)}`);
-        };
         checkPlayers(player, randomPlayer);
         i++;
         if (i <= 10) {
@@ -29,4 +31,3 @@ let myLoop = () => {
     }, 100);
 };
 myLoop();
-}
