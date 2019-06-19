59dd605285740ebaba42ceac8f5ce535ac6e3637
Obinna Odirionye <odirionye@gmail.com>
2019-06-19 10:17:14 +0100

    modified some changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-
 let player = 'rock';
 let otherPlayers: string[] = ['paper', 'scissors'];
 
 let i = 1;
 
-export const  checkMatch = (player: string, randomPlayer: string) => {
+export const checkMatch = (player: string, randomPlayer: string) => {
     if (player === 'rock' && randomPlayer === 'scissors') {
         return 'rock wins';
     } else {
@@ -13,14 +12,12 @@ export const  checkMatch = (player: string, randomPlayer: string) => {
         }
     }
 };
-export const  checkPlayers = (player: string, randomPlayer: string) => {
-    
+export const checkPlayers = (player: string, randomPlayer: string) => {
     return console.log(`${player} + ${randomPlayer} = ${checkMatch(player, randomPlayer)}`);
 };
 
 export const myLoop = () => {
     setTimeout(function() {
-      
         let randomPlayer = otherPlayers[Math.floor(Math.random() * otherPlayers.length)];
 
         checkPlayers(player, randomPlayer);
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,6 @@
 import * as application from '../src/index';
 
 describe('Get a match for rock Paper scissors', function() {
-    
     it('check match', function() {
         let result = application.checkMatch('rock', 'scissors');
         expect(result).toBe('rock wins');
@@ -20,12 +19,12 @@ it('check players', function() {
 
 it('check players', function() {
     let result = application.checkPlayers('rock', 'paper');
-    expect(result).toBeDefined
+    expect(result).toBeDefined;
 });
 
 it('check the loop', function() {
     let result = application.myLoop();
-    expect(result).toBeDefined
+    expect(result).toBeDefined;
 });
 it('check the loop', function() {
     let result = application.myLoop();
