d6db1eb9ae38559c34a6cd95f4fba330a1888aaa
Obinna Odirionye <odirionye@gmail.com>
2019-06-19 09:59:30 +0100

    test(test functions): writing unit test for functions used

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,33 @@
 import * as application from '../src/index';
 
 describe('Get a match for rock Paper scissors', function() {
+    
     it('check match', function() {
         let result = application.checkMatch('rock', 'scissors');
         expect(result).toBe('rock wins');
     });
 });
 
+it('check match', function() {
+    let result = application.checkMatch('rock', 'paper');
+    expect(result).toBe('Rock losses');
+});
+
+it('check players', function() {
+    let result = application.checkPlayers('rock', 'paper');
+    expect(result).toString;
+});
+
+it('check players', function() {
+    let result = application.checkPlayers('rock', 'paper');
+    expect(result).toBeDefined
+});
+
+it('check the loop', function() {
+    let result = application.myLoop();
+    expect(result).toBeDefined
+});
+it('check the loop', function() {
+    let result = application.myLoop();
+    expect(result).toString;
+});
