c8c87c511936b077009573f1d3de1c6f6b059d81
Obinna Odirionye <odirionye@gmail.com>
2019-06-19 02:10:40 +0100

    chore(code for interview): code running at intervals while looping.
    t

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 let player = 'rock';
-let otherPlayers: string[] = [''];
+let otherPlayers: string[] = ['paper', 'scissors'];
+
+let i = 1;
+
+let myLoop = () => {
+    setTimeout(function() {
+        let randomPlayer = otherPlayers[Math.floor(Math.random() * otherPlayers.length)];
+
+        let checkPlayers = (player: string, randomPlayer: string) => {
+            let checkMatch = (player: string, randomPlayer: string) => {
+                if (player === 'rock' && randomPlayer === 'scissors') {
+                    return 'rock wins';
+                } else {
+                    if (randomPlayer === 'paper') {
+                        return 'Rock losses';
+                    }
+                }
+            };
+            return console.log(`${player} + ${randomPlayer} = ${checkMatch(player, randomPlayer)}`);
+        };
+        checkPlayers(player, randomPlayer);
+        i++;
+        if (i <= 10) {
+            myLoop();
+        }
+    }, 100);
+};
+myLoop();
